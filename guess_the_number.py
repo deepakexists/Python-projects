e3ac1fdee023e2fd34ea63d82b4737a4e24905b1
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-01 16:43:56 -0400

    Add profect file

---
+++ guess_the_number.py
@@ -0,0 +1,21 @@
+                                                # Guess the number
+
+import random
+
+target = random.randint(1, 10)
+
+while True:
+    userChoice = input("Guss the target or Quit(Q): ")
+    if(userChoice == "Q"):
+        break
+
+    userChoice = int(userChoice)
+    if(userChoice == target):
+        print("Success : Correct Guess!!")
+        break
+    elif(userChoice < target):
+        print("Your number was too small. Take a bigger guess..")
+    else:
+        print("Your number was too big. Take a smaller guess..")
+
+print("_____GAME OVER_____")
