fbf11e9d186727639ca9c989dbb27a48f8ae7e49
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-12 18:44:58 -0700

    Add new Qs

---
+++ Python Qs/Ch-1_Qs.py
@@ -0,0 +1,35 @@
+                                      # WAP to input side of a square and print its area
+
+
+side = int(input("Enter side of a Square :"))
+
+area = side**2
+print("Area of a square is :", area)
+
+
+
+
+
+                                      # WAP to input 2 floating point numbers and print their average
+
+
+num1 = float(input("First number :"))
+num2 = float(input("Second number :"))
+
+average = (num1 + num2)/2
+print("Average :", average)
+
+
+
+
+
+
+                                     # WAP to input 2 int numbers, a and b
+                                     # print true if a is greater than or equal to b , if not print false 
+
+
+a = int(input("a :"))                     
+b = int(input("b :"))   
+
+print(a >= b)
+print(a == b or a >= b)
