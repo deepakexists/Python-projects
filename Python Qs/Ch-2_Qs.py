d88666cb053d366f69e48dbc3ec8ef5ee975c287
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-13 22:24:08 -0700

    Add new Qs

---
+++ Python Qs/Ch-2_Qs.py
@@ -0,0 +1,10 @@
+
+                        # WAP to check if a number enter by the user is odd or even.
+
+
+num = int(input("Enter a number : "))
+
+if(num%2 == 0):
+    print("even")
+else:
+    print("odd")
