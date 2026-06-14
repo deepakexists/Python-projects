d1083398d75e7fd1cb3390a81bdca6f9bebe30a8
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-13 22:25:54 -0700

    Add new Qs

--- Python Qs/Ch-2_Qs.py
+++ Python Qs/Ch-2_Qs.py
@@ -8,3 +8,23 @@ if(num%2 == 0):
     print("even")
 else:
     print("odd")
+
+
+
+
+                    # WAP to find the greatest of 3 number enter by the user
+
+
+
+a = int(input("Enter number A : "))
+b = int(input("Enter number B : "))
+c = int(input("Enter number C : "))
+
+if(a > b):
+    if(a > c):
+        print("Number A is Greater")
+elif(c > b):
+    print("Number C is Greater")
+else:
+    print("Number B is Greater")
+
