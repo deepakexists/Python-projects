b54f75bb3f12b3d2fe131ada204df56d341603d7
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-17 13:01:01 -0700

    Add new Qs

---
+++ Python Qs/Ch-3_Qs.py
@@ -0,0 +1,12 @@
+                        # WAP to ask user to enter a names of their 3 favorite movies & store them in list
+
+
+movies = []
+mov = input("Enter movie name 1 : ")
+movies.append(mov)
+mov = input("Enter movie name 2 : ")
+movies.append(mov)
+mov = input("Enter movie name 3 : ")
+movies.append(mov)
+
+print(movies)
