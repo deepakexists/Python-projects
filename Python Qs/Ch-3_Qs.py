3ee00268eb7f3172ffe2cd64086cd6ae7fbdf07d
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-18 21:58:52 -0700

    Add new Qs

--- Python Qs/Ch-3_Qs.py
+++ Python Qs/Ch-3_Qs.py
@@ -1,4 +1,4 @@
-                        # WAP to ask user to enter a names of their 3 favorite movies & store them in list
+                          # WAP to ask user to enter a names of their 3 favorite movies & store them in list
 
 
 movies = []
@@ -10,3 +10,25 @@ mov = input("Enter movie name 3 : ")
 movies.append(mov)
 
 print(movies)
+
+
+
+
+
+
+                      # WAP to check if a list contains of palindrome of elements. (Hint: use copy() method)
+
+list = [1, 2, 2, 1]
+print(list)
+
+copy_list = list.copy()
+copy_list.reverse()
+print(copy_list)
+
+if(copy_list == list):
+    print("Its a palindrome")
+else:
+    print("not palidrome")
+
+
+
