44185deb3763eaeef1ecb46be3e8035661487f5d
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-20 10:49:05 -0700

    Add new Qs

---
+++ Python Qs/Ch-4_Qs.py
@@ -0,0 +1,10 @@
+                                     # Store following word meanings in a python Dictionary :
+                                     #  table : "a piece of furniture", "list of facts & figures"
+                                     #  cat : "a small animal"
+
+dict = {
+    "table" : ("a piece of furniture", "list of facts & figures"),
+    "cat" : "a small animal"
+}   
+
+print(dict)
