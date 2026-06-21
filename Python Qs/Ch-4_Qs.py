fdcedc427dd700722f4d0d6150b82bbd9a523c29
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-21 06:17:39 -0700

    Add new Qs

--- Python Qs/Ch-4_Qs.py
+++ Python Qs/Ch-4_Qs.py
@@ -8,3 +8,19 @@ dict = {
 }   
 
 print(dict)
+
+
+
+
+
+
+
+                               #     you are given a list of students. Assume one classroom is requred for 1 subject.
+                               #     How many classrooms are required for all the students.
+                               #         "python", "java", "c++", "python", "javascript", 
+                               #          "java", "python", "java", "c++", "c"
+
+
+subjects = { "python", "java", "c++", "python", "javascript", "java", "python", "java", "c++", "c"}
+print(subjects)
+print(len(subjects))
