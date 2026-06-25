c834c1d6b5e28e4f80d4771e92cd2042f75fe297
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-06-25 14:10:27 -0700

    Add new Qs

--- Python Qs/Ch-5_Qs.py
+++ Python Qs/Ch-5_Qs.py
@@ -4,3 +4,12 @@ num = 1
 while num <= 100:   # Stoping condition
     print(num)
     num+=1
+
+
+
+
+                                           #  print number from 100 to 1
+num = 100
+while num >= 1:
+    print(num)
+    num-=1
