ab370f1903914517ee3e7ed834206851c8d9a5ad
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-07-02 23:04:40 +0530

    Add new Qs

--- Python Qs/Ch-5_Qs.py
+++ Python Qs/Ch-5_Qs.py
@@ -116,3 +116,27 @@ n = int(input("Enter a number: "))
 for i in range(1, 11):
     print(n*i)
 
+
+
+
+
+                                # WAP to find the sum of n natural number. (using while loop)
+
+While loop
+n = int(input("Enter a number: "))
+
+sum = 0
+i = 1
+while(i <= n):
+    sum += i
+    i += 1
+print("Sum is : ", sum )
+
+
+For loop
+n = int(int(input("Enter a number: ")))
+
+sum = 0
+for i in range(1, n+1):
+    sum += i
+print(sum)
