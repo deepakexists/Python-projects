b313adc1d8f82e4b5744921f1e8b8ea31fd4477a
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-07-05 15:04:19 +0530

    Add new Qs

--- Python Qs/Ch-6_Qs.py
+++ Python Qs/Ch-6_Qs.py
@@ -10,3 +10,20 @@ def calc_avg(a, b, c):
     return(avg)
 
 calc_avg(2, 4, 3)
+
+
+
+
+
+
+
+                                    # WAF to print the length of a list. (list in the parameter)
+
+num = [1, 2, 3, 4, 6]
+name = ["Deepak", "Harsh", "tarun", "Aaryan"]
+
+def len_list(list):
+    print(len(list))
+
+len_list(num)
+len_list(name)
