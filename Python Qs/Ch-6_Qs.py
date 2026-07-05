54712c0a194492e20c5a1fd94c834cbdccc225de
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-07-05 15:02:55 +0530

    Add new Qs

---
+++ Python Qs/Ch-6_Qs.py
@@ -0,0 +1,12 @@
+
+                                                            # Function
+
+                                       # Average of 3 numbers
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return(avg)
+
+calc_avg(2, 4, 3)
