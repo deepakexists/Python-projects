aebfa30c317312076b2268aeaa8f387f02e1c98a
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-07-08 10:13:10 +0530

    Add new Qs

--- Python Qs/Ch-6_Qs.py
+++ Python Qs/Ch-6_Qs.py
@@ -56,3 +56,14 @@ def print_fact(n):
 
 print_fact(5)
 print_fact(5)
+
+
+
+             
+                                               # WAF to convert USD to INR.
+
+def converter(usd_val):
+    inr_val = usd_val * 90
+    print(usd_val, "USD = ", inr_val, "INR")
+
+converter(5)    
