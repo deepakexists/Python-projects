0960fce1d5cdb37e5a17eda758267fa456f1a3e2
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-07-12 21:42:29 +0530

    Add new Qs

---
+++ Python Qs/ch-7_Qs.py
@@ -0,0 +1,14 @@
+                              #    create a new file "practice.txt" using python. Add the following data in it:
+                              #                        Hi everyone
+                              #                        We are learning file I/o
+                              #                        using Java
+                              #                        I like programing in Java
+
+
+with open("practice.txt", "w") as f:
+    f.write("Hi everyone \n")
+    f.write("We are learning file I/O \n")
+    f.write("using Java \n")
+    f.write("I like programing in Java")
+
+
