2cd2d394c49625f25a164846b432befa53cd2a62
Deepak Bansal <bansaldeepak64993@gmail.com>
2026-07-17 17:23:42 +0530

    Add new Qs

--- Python Qs/Ch-8_Qs.py
+++ Python Qs/Ch-8_Qs.py
@@ -16,3 +16,39 @@ s1 = student("Deepak", 98, 65, 87)
 print(s1.name, s1.marks1, s1.marks2, s1.marks3)    
 
 print(s1.marks_average())
+
+
+
+
+
+                                        # Create account with 2 attributes = balance & account no. 
+                                        # create method for debit, credit, print the balance.
+
+ 
+class account:
+    def __init__(self, balance, account_no):
+        self.balance = balance
+        self.account_no = account_no
+
+    # debit method  
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs. ", amount, " was debited")
+        print("Total Balance = ", self.get_balance())
+
+    # credit method  
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs. ", amount, " was credited")
+        print("Total Balance = ", self.get_balance())
+
+    # balance
+    def get_balance(self):
+        return self.balance
+
+
+acc1 = account(10000, 12345)
+acc1.debit(1000)
+acc1.credit(500)
+
+
